bcd0c06a3008430068ded43e244de2d31dd4358f
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-07-04 10:08:18 +0430

    done

--- src/main.py
+++ src/main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import numpy as np
+import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer as sia
+import time
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+start_time = time.time()
+interactions = pd.read_excel('..\interactions.xlsx')
 
+interactions = interactions.head(1000)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+analyser = sia()
+scores = []
+text = interactions['Extracted Interaction Text']
 
+def analyse(row):
+    return analyser.polarity_scores(row['Extracted Interaction Text'])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+interactions['score'] = interactions.apply(analyse, axis=1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+counter = 1
+for  sentence in text:
+    score = analyser.polarity_scores(sentence)
+    scores.append(score)
+    counter += 1
+
+end_time = time.time()
+print(end_time - start_time)
+# Converting List of Dictionaries into Dataframe
+dataFrame = pd.DataFrame(scores)
+# dataFrame.head(20)
+
+for em in interactions.fromEmailId.unique():
+    #interactions.loc[(interactions['fromEmailId']==em),'compound']=(interactions['compound'].where(interactions['fromEmailId']==em)).mean()
+    sales_data.loc[sales_data['SalesAgentEmailID']==em,'interaction_score']=(interactions['compound'].where(interactions['fromEmailId']==em)).mean()
